3bab5159101ceaac2fcd727b4d3d12f9aa2d11c5
Diego <creacioneszaide424@gmail.com>
2024-10-27 13:49:26 -0500

    creo la salida por document

--- script.js
+++ script.js
@@ -5,16 +5,21 @@ let altura = parseFloat(prompt("ingrese su altura"));
 //creo la variable de imc donde voy a dividir el peso y la altura al cuadrado
 let imc = peso / (altura * altura);
 
-//creo las condicinales que terminando los parametros
-if (imc < 19.5 ){
+//creo las condicinales que determinan los parametros
+if (imc < 18.5 ){
     clasificacion = "bajo peso";
 
-} else if (imc >= 19.5 && imc < 45.5){
+} else if (imc >= 18.5 && imc < 24.9){
     clasificacion = "peso normal";
 
-}else if (imc >=45.5 && imc < 60.5 ){
-    clasificacion "sobrepeso";
+}else if (imc >=25 && imc < 29.9 ){
+    clasificacion = "sobrepeso";
 
 }else {
     clasificacion = "obesidad"
 }
+
+//muestro el resultado de imc por document.write
+document.write("tu imc es:" + imc.toFixed(2)+"<br>");
+document.write("clasificacion" + clasificacion);
+ 

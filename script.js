2c0e29beed69aab1cf4b044b4426c0cc362b1445
Diego <creacioneszaide424@gmail.com>
2024-10-27 11:31:40 -0500

    creo las condicionales

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ let altura = parseFloat(prompt("ingrese su altura"));
 
 //creo la variable de imc donde voy a dividir el peso y la altura al cuadrado
 let imc = peso / (altura * altura);
+
+//creo las condicinales que terminando los parametros
+if (imc < 19.5 ){
+    clasificacion = "bajo peso";
+
+} else if (imc >= 19.5 && imc < 45.5){
+    clasificacion = "peso normal";
+
+}else if (imc >=45.5 && imc < 60.5 ){
+    clasificacion "sobrepeso";
+
+}else {
+    clasificacion = "obesidad"
+}
